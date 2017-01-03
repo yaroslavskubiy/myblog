2252ab9478aa46ff741fcab26f040f9987815f1f
yaroslavskubiy <yaroslavskubiy@gmail.com>
2017-01-04 00:41:59 +0200

    Проверяем работоспособность БД

---
+++ index.php
@@ -0,0 +1,19 @@
+﻿<?php
+$server = 'localhost';
+$user = 'myblog';
+$password = '12345';
+
+$dblink = mysql_connect($server, $user, $password);
+
+if($dblink)
+echo 'Соединение установлено.';
+else
+die('Ошибка подключения к серверу баз данных.');
+
+$database = 'myblog';
+$selected = mysql_select_db($database, $dblink);
+if($selected)
+echo ' Подключение к базе данных прошло успешно.';
+else
+die(' База данных не найдена или отсутствует доступ.');
+?>
