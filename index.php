59a24c6075a11eb75d19a8a982528276725451e5
yaroslavskubiy <yaroslavskubiy@gmail.com>
2017-01-04 00:51:46 +0200

    Создаем главную страницу сайта для  выводиа статьи.

--- index.php
+++ index.php
@@ -1,19 +1,48 @@
 ﻿<?php
-$server = 'localhost';
-$user = 'myblog';
-$password = '12345';
-
-$dblink = mysql_connect($server, $user, $password);
-
-if($dblink)
-echo 'Соединение установлено.';
-else
-die('Ошибка подключения к серверу баз данных.');
-
-$database = 'myblog';
-$selected = mysql_select_db($database, $dblink);
-if($selected)
-echo ' Подключение к базе данных прошло успешно.';
-else
-die(' База данных не найдена или отсутствует доступ.');
+ 
+require( "config.php" );
+$action = isset( $_GET['action'] ) ? $_GET['action'] : "";
+ 
+switch ( $action ) {
+  case 'archive':
+    archive();
+    break;
+  case 'viewArticle':
+    viewArticle();
+    break;
+  default:
+    homepage();
+}
+ 
+ 
+function archive() {
+  $results = array();
+  $data = Article::getList();
+  $results['articles'] = $data['results'];
+  $results['totalRows'] = $data['totalRows'];
+  $results['pageTitle'] = "Article Archive | Widget News";
+  require( TEMPLATE_PATH . "/archive.php" );
+}
+ 
+function viewArticle() {
+  if ( !isset($_GET["articleId"]) || !$_GET["articleId"] ) {
+    homepage();
+    return;
+  }
+ 
+  $results = array();
+  $results['article'] = Article::getById( (int)$_GET["articleId"] );
+  $results['pageTitle'] = $results['article']->title . " | Widget News";
+  require( TEMPLATE_PATH . "/viewArticle.php" );
+}
+ 
+function homepage() {
+  $results = array();
+  $data = Article::getList( HOMEPAGE_NUM_ARTICLES );
+  $results['articles'] = $data['results'];
+  $results['totalRows'] = $data['totalRows'];
+  $results['pageTitle'] = "Widget News";
+  require( TEMPLATE_PATH . "/homepage.php" );
+}
+ 
 ?>
