057ae4f45cd2de9f449739ad75e3f74b3b416ae9
yaroslavskubiy <yaroslavskubiy@gmail.com>
2017-01-04 02:33:54 +0200

    Создали шапку со встрояным java. Сама java еще не работает

---
+++ templates/include/header.php
@@ -0,0 +1,86 @@
+<html lang="en-US">
+<head>
+	<title><?php echo htmlspecialchars( $results['pageTitle'] )?></title>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" href="templates/css/style.css" type="text/css" media="all" />
+	<link rel="stylesheet" href="templates/css/jquery.jcarousel.css" type="text/css" media="all" />
+	
+	<link rel="shortcut icon" href="templates/css/images/favicon.ico" />
+	<script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
+	<script type="text/javascript" src="js/jquery.jcarousel.pack.js"></script>
+	<script type="text/javascript" src="js/func.js"></script>
+</head>
+<body>
+<div class="shell">
+	<div class="border">
+		<div id="header">
+			<h1 id="logo"><a href="#" class="notext">beSmart</a></h1>
+			<div class="socials right">
+				<ul>
+				    <li><a href="#" class="rss">RSS</a></li>
+				    <li><a href="#" class="fb">Facebook</a></li>
+				    <li class="last"><a href="#" class="twit">Twitter</a></li>
+				</ul>
+			</div>
+			<div class="cl">&nbsp;</div>
+		</div>
+		
+		<div id="navigation">
+			<ul>
+			    <li><a href="#" class="active">Home</a></li>
+			    <li><a href="#">About</a></li>
+			    <li><a href="#">Services</a></li>
+			    <li><a href="#">Projects</a></li>
+			    <li><a href="#">Blog</a></li>
+			    <li><a href="#">Contact</a></li>
+			</ul>
+			<div class="cl">&nbsp;</div>
+		</div>
+		
+		<div class="slider">
+			<div class="slider-nav">
+				<a href="#" class="left notext">1</a>
+				<a href="#" class="left notext">2</a>
+				<a href="#" class="left notext">3</a>
+				<a href="#" class="left notext">4</a>
+				<div class="cl">&nbsp;</div>
+			</div>
+			<ul>
+			    <li>
+			    	<div class="item">
+						<div class="text">
+							<h3><em>achieve your</em></h3>
+							<h2><em>TARGETS</em></h2>
+						</div>
+			    		<img src="templates/css/images/slider01.gif" alt="" />
+			    	</div>
+			    </li>
+			    <li>
+			    	<div class="item">
+						<div class="text">
+							<h3><em>achieve your</em></h3>
+							<h2><em>TARGETS</em></h2>
+						</div>
+			    		<img src="templates/css/images/slider01.gif" alt="" />
+			    	</div>
+			    </li>
+			    <li>
+			    	<div class="item">
+						<div class="text">
+							<h3><em>achieve your</em></h3>
+							<h2><em>TARGETS</em></h2>
+						</div>
+			    		<img src="templates/css/images/slider01.gif" alt="" />
+			    	</div>
+			    </li>
+			    <li>
+			    	<div class="item">
+						<div class="text">
+							<h3><em>achieve your</em></h3>
+							<h2><em>TARGETS</em></h2>
+						</div>
+			    		<img src="templates/css/images/slider01.gif" alt="" />
+			    	</div>
+			    </li>
+			</ul>
+		</div>
